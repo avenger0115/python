d7ec0a50d173d06cb2e07e8774badc9ee3e5362c
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-01-04 20:54:45 +0900

    2024년1월4일

--- abc.py
+++ abc.py
@@ -1,30 +1,74 @@
-import random as rd
-import turtle
+##a = 'python'
+##b = 'is fun'
+##print(a+b)
+##print(a*2)
+##a = [1,2,3,]
+##b = [4,5,6]
+##print(a+b)
+##print(a*3)
+##a = '*' 
+##print(a*10)
+##a = "carpe Diem!"
+##print(a[6:11])
+##a = "carpe Diem!"
+##print(a[6:])
+##a = "carpe Diem!"
+##print(a[-5:-1])
+##a = "carpe Diem!"
+##print(a[0:5])
+##a ='l love you'
+##print(a.count('o'))
+##print(a.find('y'), a.index('y'))
+##print('.'.join(a))
+##print(a.split('v'))
+##print(a.replace('love','like'))
+##print(a.upper(), a.lower())
+##a = ' sbcd '
+##b = 'efgh'
+##print('%s%s' %(a, b))
+##a = a.strip()
+##print('%s%s' %(a, b))
+##a = 'hello world'
+##print(a.count('l'))
+##b = 'python'
+##print(b.find('p'))
+##c = 'oh!my!god'
+##print(c.split('!'))
+##a = [1,5,10,3]
+##a.append(20)
+##print(a)
+##a.sort()
+##print(a)
+##a = ['b','a','n','a','n','a']
+##a.reverse()
+##print(a)
+##a.remove('a')
+##print(a)
+##p = a.pop()
+##print(p,a)
+##n = a.count('n')
+##print(n)
+num =  input('주민 번호 : ')
+a = num[:6]
+b = num[7]
+print('생년월일 : %s '  % a)
+print('성별 : %s ' % b)
 
-t = turtle.Turtle()
-t.shape('turtle')
 
-x = rd.randint(-300,300)
-y = rd.randint(-300,300)
 
-t.goto(x,y)
 
-x = rd.randint(-300,300)
-y = rd.randint(-300,300)
 
-t.goto(x,y)
 
-x = rd.random() + rd.randint(-300,300)
-x = rd.random() + rd.randint(-300,300)
 
-t.goto(x,y)
 
-x = rd.random() + rd.randint(-300,300)
-x = rd.random() + rd.randint(-300,300)
 
-t.goto(x,y)
 
-turtle.done()
+
+
+
+
+
+
 
 
 
