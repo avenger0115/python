81703f7f953547eabfe84b5255e19fc55faf63d9
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-01-25 20:54:45 +0900

    2024 01 25

--- abc.py
+++ abc.py
@@ -1,53 +1,141 @@
-##1.
-##a = int (input("inch : "))
-##print("%fcm"%(a*2.54))
-##2.
-##a = int (input("inch : "))
-##b = a*2.54
-##print(b)
-##x = 10
-##y = 50
-##print('x : %d, y : %d' %(x,y))
-##print('x와 y는같다?', x==y)
-##print('x와 y는 다르다?',x !=y)
-##print('x는 y보다 크다?',x > y)
-##print('x는 y보다 작다?', x <y)
-##print('x는 y보다 크거나 같다?', x >=y)
-##print('x는 y보다 작거나 같다?', x<=y)
-##a = int(input())
-##b = int(input())
-##c = int(input())
-##print(a>=140 and b>=140 and c>=140)
-##score = int(input('score : '))
-##if score >= 60 : 
-##    print('Pass')
+##import random
+##com = random.randint(1,2)
+##user = int(input('odd : 1, even :2\n'))
+##print('com(%d) : user(%d)'%(com,user))
+##if com == user :
+##    print('correct')
 ##else :
-##a = int(input())
-##if a%2==0 :
-##    print('EVEN')
+##    print('lncorrect')
+##import random
+##a = ['월','화','수','목','금','토','일',]
+##b = random.randint(0,6)
+##print(a[b])
+##from random import *
+##a= ['월','화','수','목','금']
+##print(choice([True,False]))
+##print(choices(a))
+##from random import *
+##a = choice('abcdefg')
+##print(a)
+##from random import *
+##a = [1,2,3,4,5]
+##print(choices(a,[1,1,10,1,1]))
+##from random import *
+##a=[1,2,3,4,5,]
+##print(choices(a,[1,2,1,1,5],k=3))
+##from random import *
+##a=[0,1,2,3,4,5]
+##b = choice(a)
+##if b == 0:
+##    print('loss!')
 ##else :
-##    print('ODD')
-##score=input("성별을 입력하세요. : ")
-##if score == 'M' :
-##    print('Man')
-##elif score == 'W' :  
-##    print('Woman')
-##else :
-##    print('what')
-##score = input('월을 입력하세요. : ')
-##score = int(score)
-##if score>=1 and score<=6 :
-##    print('first haif')
-##elif score>=7 and score<=12 :
-##    print('second haif')
-score = input('다음과 같은 메뉴가 있습니다. : ')
-score = int(score)
-if score == 1 :
-    print('burgers are not available')
-elif score>=2 and score<=3 :
-    print('what would you like to drink?')
-elif score == 4 :
-    print('l like coke, too.',)
-else :
-    print('would you like hot or cold?')
+##    print('no,%d spot!')
+##from random import *
+##a = [1,2,3,4,5,6,7,8,9,10]
+##b = choices(a,[2,2,2,2,2,2,1,2,2,2],k=3)
+##if b[0]==7 and b[1]==7 and b[2]==7:
+##    print('lucky!')
+from random import *
+import turtle
+house = turtle.Turtle()
+house.penup()
+house.goto(300,-100)
+house.pendown()
+house.fillcolor("skyblue")
+house.begin_fill()
+house.forward(100)
+house.left(90)
+house.forward(100)
+house.left(90)
+house.forward(100)
+house.left(90)
+house.forward(100)
+house.end_fill()
+house.fillcolor("royalblue")
+house.begin_fill()
+house.backward(100)
+house.left(180)
+house.left(-30)
+house.forward(100)
+house.right(120)
+house.forward(100)
+house.right(120)
+house.forward(100)
+house.end_fill()
+line = turtle.Turtle()
+line.penup()
+line.goto(-380,-100)
+line.pendown()
+line.write(0)
+line.forward(340)
+line.write(50)
+line.forward(340)
+line.write(100)
+t = turtle.Turtle(shape = 'turtle')
+t.penup()
+t.goto(-400,-100)
+t.pendown()
+t.color("purple","pink")
+t.penup()
+g =turtle.Turtle()
+g.write("씨큐브 코딩의 타자 게임!", True, font=("Arial",20,"bold"))
+fruit = ["apple","banana", "strawberry","watermelon","mandarin"]
+score = 0
+n = randint(3,5)
+for i in range(n):
+   s = choice(fruit)
+   word = turtle.textinput("fruit","%s(%d/%d)"%(s,i+1,n))
+   if s ==word :
+       score +=1
+rate = score / n *100
+g.penup()
+g.goto(0,-50)
+g.pendown()
+g.write("%d/%d번 성공!" %(score,n), True,font = ("Arial",15,"bold"))
+g.penup()
+g.goto(0,-100)
+g.pendown()
+g.write("정확도 : %1f%%" % rate,True,font =("Arial", 15, "bold"))
+
+
+
+
+
+        
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
