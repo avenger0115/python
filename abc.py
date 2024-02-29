d426cf4de951c1e0cdd631bacf68e20a599c317a
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-02-29 20:53:07 +0900

    02-29

--- abc.py
+++ abc.py
@@ -3,89 +3,151 @@
 ##from pygame.locals import *
 ##
 ##pygame.init()
-##SCREEN = pygame.display.set_mode((300,300))
+##SCREEN = pygame.display.set_mode((200,200))
 ##CLOCK = pygame.time.Clock()
 ##
-##BLACK = (0,0,0)
-##WHITE = (255,255,255)
+##img1 = pygame.image.load("light.png")
+##img2 = pygame.image.load("man.png")
 ##
-##sysfont = pygame.font.SysFont(None,36)
+##img1_size = img1.get_size()
+##img2_size = img2.get_size()
 ##
-##txt = sysfont.render("",True,BLACK)
+##print(img1_size)
+##print(img2_size)
 ##
 ##while True :
-##    SCREEN.fill(WHITE)
 ##
+##     SCREEN.fill((255,255,255))
+##
+##     for event in pygame.event.get() :
+##         if event.type == QUIT :
+##             pygame.quit()
+##             sys.exit()
+##
+##     SCREEN.blit(img1,(25,40))
+##     SCREEN.blit(img2,(100,100))
+##     pygame.display.update()
+##     CLOCK.tick(1)
+##
+##import sys
+##import pygame
+##from pygame.locals import *
+##
+##pygame.init()
+##SCREEN = pygame.display.set_mode((300,300))
+##CLOCK = pygame.time.Clock()
+##sysfont = pygame.font.SysFont(None,30)
+##txt = "Wait..."
+##txt2 = "Key Down! "
+##
+##while True :
+##    SCREEN.fill ((255,255,255))
 ##    for event in pygame.event.get() :
 ##        if event.type == QUIT :
 ##            pygame.quit()
 ##            sys.exit()
-##            
-##        if event.type == KEYDOWN:
-##            
-##            if event.key == K_UP :
-##                txt = sysfont.render("UP", True, BLACK) 
+##    Key_event = pygame.key.get_pressed()
 ##
-##            if event.key == K_DOWN :
-##                txt = sysfont.render("DOWN", True, BLACK) 
+##    if Key_event[pygame.K_LEFT]:
+##        txt = "Left"+txt2
+##        
+##    if Key_event[pygame.K_RIGHT]:
+##        txt = "Right"+txt2
 ##
-##            if event.key == K_LEFT :
-##                txt = sysfont.render("LEFT", True, BLACK) 
+##    if Key_event[pygame.K_UP]:
+##        txt = "UP"+txt2
 ##
-##            if event.key == K_RIGHT :
-##                txt = sysfont.render("RIGHT", True, BLACK) 
+##    if Key_event[pygame.K_DOWN]:
+##        txt = "Down"+txt2
 ##
-##            if event.key == K_ESCAPE :
-##               pygame.quit()
-##               sys.exit()
-##
-##    SCREEN.blit(txt, (100,120))
+##    if Key_event[pygame.K_ESCAPE]:
+##        pygame.quit()
+##        sys.exit()
+##         
+##    msg = sysfont.render(txt,True,(0,0,0))
+##    SCREEN.blit(msg,(50,100))  
 ##    pygame.display.update()
 ##    CLOCK.tick(60)
-        
+
 import sys
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
-SCREEN = pygame.display.set_mode((300,300))
+SCREEN = pygame.display.set_mode((600,600))
 CLOCK = pygame.time.Clock()
+sysfont = pygame.font.SysFont(None,72)
 
+light_img = pygame.image.load("light.png")
+man_img = pygame.image.load("man.png")
 
-sysfont = pygame.font.SysFont(None,36)
+l_size = light_img.get_size()
+m_size = man_img.get_size()
 
-txt = sysfont.render("0",True,(0,0,0))
+game_over = False
 
-n = 0
+l_x = [ ]
+l_y = [ ]
+m_x,m_y = 250,480
+cnt = 0
 
 while True :
-    SCREEN.fill((255,255,255))
-    
-    for event in pygame.event.get() :
-        if event.type == QUIT :
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
 
-            if event.key == K_UP :  
-                 n +=10
-            if event.key == K_DOWN :
-                 n-=10
-            if event.key == K_LEFT :
-                 n *=10
-            if event.key == K_RIGHT :
-                 n //=10
-            if event.key == K_ESCAPE :
-                pygame.quit()
-                sys.exit()
-        if event.type == MOUSEBUTTONDOWN :
-           if event.button == 1:
-               n =0
+     if game_over :
+          break
+     SCREEN.fill((0,0,0))
+     for event in pygame.event.get() :
+         if event.type == QUIT :
+             pygame.quit()
+             sys.exit()
 
-    txt = sysfont.render('%d' % n, True, (0,0,0))
-    SCREEN.blit(txt, (100,120))
-    pygame.display.update()
-    CLOCK.tick(60)
+     for i in range(len(l_x)) :
+          if l_x[i] + l_size[0] >=m_x and m_x+m_size[0]>=l_x[i]:
+               if l_y[i]- l_size[1] >= m_y :
+                    msg = sysfont.render("Game Over!",True,(255,0,0))
+                    SCREEN.blit(msg,(160,250))
+                    game_over = True
+
+     key_event = pygame.key.get_pressed()
+     if key_event[pygame.K_RIGHT]:
+          if m_x <500:
+              m_x +=5
+
+     if key_event[pygame.K_LEFT]:
+          if m_x >0:
+              m_x -=5
+              
+     SCREEN.blit(man_img,(m_x,m_y))  
+
+     cnt +=1
+     if cnt >=20 :
+         cnt = 0
+         l_x.append(random.randint(0,600))
+         l_y.append(0)
+
+     for i in range(len(l_x)) :
+
+          l_y[i]+=5
+   
+          SCREEN.blit(light_img,(l_x[i],l_y[i]))
+
+          if l_y[i] >= 550 :
+              l_x.remove(l_x[i])
+              l_y.remove(l_y[i])
+              break
+
+     pygame.display.update()
+     CLOCK.tick(60)
+
+   
+
+
+
+
+
+
+
 
 
 
