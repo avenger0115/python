2b80dc58305049c8bcfb7e4d63ec22f66476d2ff
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-03-07 20:52:41 +0900

    2024-03-07

--- abc.py
+++ abc.py
@@ -1,153 +1,87 @@
-##import sys
-##import pygame
-##from pygame.locals import *
+##def add(x,y) :
+##    s = x+y
+##    return s
 ##
-##pygame.init()
-##SCREEN = pygame.display.set_mode((200,200))
-##CLOCK = pygame.time.Clock()
+##result = add(10,20) + add(20,30)
+##print(result)
+##def get_max(x,y) :
+##    if x > y :
+##        return x
+##    else :
+##        return y
 ##
-##img1 = pygame.image.load("light.png")
-##img2 = pygame.image.load("man.png")
+##x =int(input())
+##y = int(input())
 ##
-##img1_size = img1.get_size()
-##img2_size = img2.get_size()
+##n = get_max(x,y)
+##print(n)
+
+##def plus(x) :
+##    if x > 0 :
+##       return True
+##    else :
+##       return False
 ##
-##print(img1_size)
-##print(img2_size)
+##x = int(input("정수를 입력하세요.:"))
+##print(plus(x))
+        
+##def get_sum(n) :
+##    s = 0
+##    for i in range(1,n+1) :
+##        s+= i
+##    return s
 ##
-##while True :
+##n = int(input())
+##print("1부터 %d까지의 합은 %d입니다." %(n,get_sum(n)))
+
+##def get_sum(a,b) :
+##    s = 0
+##    for i in range(a,b+1) :
+##        s+= i
+##    return s
+##    
+##a = int(input())
+##b = int(input())
+##print("%d부터 %d까지의 합은 %d입니다." %(a,b,get_sum(a,b)))
+
+##def area(x,y) :
+##    s = x*y
+##    return s
+##    
+##w = int(input("가로입력 :"))
+##h = int(input("세로입력 :"))     
 ##
-##     SCREEN.fill((255,255,255))
+##n = area(w,h)
+##print(n)
+
+##def area(w) :
+##    s = 3.14*w*w
+##    return s
 ##
-##     for event in pygame.event.get() :
-##         if event.type == QUIT :
-##             pygame.quit()
-##             sys.exit()
+##w = int(input("반지름 입력 : "))
 ##
-##     SCREEN.blit(img1,(25,40))
-##     SCREEN.blit(img2,(100,100))
-##     pygame.display.update()
-##     CLOCK.tick(1)
+##n = area(w)
+##print(n)
+
+##def number(w) :
+##    if w % 2 == 0 :
+##        return "even"
+##    else :
+##        return "odd"
 ##
-##import sys
-##import pygame
-##from pygame.locals import *
+##w = int(input("정수를 입력하세요:"))
 ##
-##pygame.init()
-##SCREEN = pygame.display.set_mode((300,300))
-##CLOCK = pygame.time.Clock()
-##sysfont = pygame.font.SysFont(None,30)
-##txt = "Wait..."
-##txt2 = "Key Down! "
-##
-##while True :
-##    SCREEN.fill ((255,255,255))
-##    for event in pygame.event.get() :
-##        if event.type == QUIT :
-##            pygame.quit()
-##            sys.exit()
-##    Key_event = pygame.key.get_pressed()
-##
-##    if Key_event[pygame.K_LEFT]:
-##        txt = "Left"+txt2
-##        
-##    if Key_event[pygame.K_RIGHT]:
-##        txt = "Right"+txt2
-##
-##    if Key_event[pygame.K_UP]:
-##        txt = "UP"+txt2
-##
-##    if Key_event[pygame.K_DOWN]:
-##        txt = "Down"+txt2
-##
-##    if Key_event[pygame.K_ESCAPE]:
-##        pygame.quit()
-##        sys.exit()
-##         
-##    msg = sysfont.render(txt,True,(0,0,0))
-##    SCREEN.blit(msg,(50,100))  
-##    pygame.display.update()
-##    CLOCK.tick(60)
+##n = number(w)
+##print(n)
 
-import sys
-import pygame
-from pygame.locals import *
-import random
+def square(a,b) :   
+    return a **b
+        
+a = int(input("a 를 입력 : "))
+b = int(input("b 를 입력 : "))
 
-pygame.init()
-SCREEN = pygame.display.set_mode((600,600))
-CLOCK = pygame.time.Clock()
-sysfont = pygame.font.SysFont(None,72)
-
-light_img = pygame.image.load("light.png")
-man_img = pygame.image.load("man.png")
-
-l_size = light_img.get_size()
-m_size = man_img.get_size()
-
-game_over = False
-
-l_x = [ ]
-l_y = [ ]
-m_x,m_y = 250,480
-cnt = 0
-
-while True :
-
-     if game_over :
-          break
-     SCREEN.fill((0,0,0))
-     for event in pygame.event.get() :
-         if event.type == QUIT :
-             pygame.quit()
-             sys.exit()
-
-     for i in range(len(l_x)) :
-          if l_x[i] + l_size[0] >=m_x and m_x+m_size[0]>=l_x[i]:
-               if l_y[i]- l_size[1] >= m_y :
-                    msg = sysfont.render("Game Over!",True,(255,0,0))
-                    SCREEN.blit(msg,(160,250))
-                    game_over = True
-
-     key_event = pygame.key.get_pressed()
-     if key_event[pygame.K_RIGHT]:
-          if m_x <500:
-              m_x +=5
-
-     if key_event[pygame.K_LEFT]:
-          if m_x >0:
-              m_x -=5
-              
-     SCREEN.blit(man_img,(m_x,m_y))  
-
-     cnt +=1
-     if cnt >=20 :
-         cnt = 0
-         l_x.append(random.randint(0,600))
-         l_y.append(0)
-
-     for i in range(len(l_x)) :
-
-          l_y[i]+=5
-   
-          SCREEN.blit(light_img,(l_x[i],l_y[i]))
-
-          if l_y[i] >= 550 :
-              l_x.remove(l_x[i])
-              l_y.remove(l_y[i])
-              break
-
-     pygame.display.update()
-     CLOCK.tick(60)
-
-   
-
-
-
-
-
-
-
+n = square(a,b)
+print(n)
 
 
 
