f18f9e6b9998bd446b4c17c52026d4a79a09653d
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-03-21 20:50:31 +0900

    2024-03-21

--- abc.py
+++ abc.py
@@ -1,142 +1,68 @@
-##def square(a,b) :
-##    return a **b
+##from tkinter import *
+##win = Tk()
 ##
-##a = int(input("a를 입력 : "))
-##b = int(input("b 를 입력 : "))
+##img = PhotoImage(file = "pizza.png")
+##hihi = Label(win,image = img)
+##abc = Label(win,text = "pizza",bg = "yellow",fg = "red")
+##hihi.pack()
+##abc.pack()
 ##
-##n = square(a,b) 
-##print(n)
 ##
-##def warning() :
-##    for _ in range(3) :
-##        print("Fier!")
-##warning()
+##win.mainloop()
+
+##from tkinter import *
+##win = Tk()
 ##
-##def two_times() :
-##    for i in range(1,10) :
-##        print("2 * %d = %d" % (i, 2* i), end = ' ')
-##two_times()
+##apple = Button(win,text = "Button")
+##apple.pack()
+##win.mainloop()
+
+##from tkinter import *
+##win = Tk()
 ##
-##def say(name) :
-##    print("Welcome,", name)
-##    return
-##say("minsu")
+##def click():
+##    if btn['text'] == "red":
+##        btn['text'] = "blue"
+##        btn['bg'] = "blue"
+##    else:
+##        btn['text'] = "red"
+##        btn['bg'] = "red"
 ##
-##def day(m,d) :
-##    print("Today is %s/%s." %(m,d))
-##day(12,7)
+##btn = Button(win,text = "red",fg="white",bg ="red",command=click)
+##btn.pack()
 ##
-##a = 0
-##def f() :
-##    global a
-##    global b
-##    print(a)
-##    a = 10
-##    b = 20
-##f()
-##print(a)
-##print(b)
+##win.mainloop()
+
+##from tkinter import *
+##win = Tk()
 ##
-##def A() :
-##    print(1)
-##    r = B()
-##    print(r)
-##def B() :
-##    print(2)
-##    return 3
-##A()
-##print(4)
+##def click():
+##    if hihi['text'] == "hello":
+##        hihi['text'] = "python"
+##        hihi['bg'] = "green"
+##    else:
+##        hihi['text'] = "hello"
+##        hihi['bg'] = "orange"
+##        
+##hihi = Label(win,text = "hello",bg="orange",fg ="white")
+##btn = Button(win,text = "Button", command=click)
+##hihi.pack()
+##btn.pack()
 ##
-##def f(n) :
-##     print(n)
-##     if n > 1 :
-##         f(n-1)
-##f(3)
-##
-##def factorial(n) :
-##    if n == 1 :
-##        return 1
-##    else :
-##        return n * factorial(n-1)
-##
-##fac = factorial(4)
-##print("4! =", fac)
-## 
-##def judge(n) :
-##    if n > 0 :
-##        print("plus")
-##    elif n < 0 :
-##        print("minus")
-##    else :
-##        print("zero")
-##
-##n = int(input())
-##judge(n)
-##
-##def season(n) :
-##    if n > 2 and n <6 :
-##        print("spring")
-##    elif n > 5 and n < 9 :
-##        print("summer")
-##    elif n > 8 and n < 12 :
-##        print("fall")
-##    else :
-##        print("winter")
-##
-##n = int(input("월 입력"))
-##season(n)
+##win.mainloop()
 
-from random import *
+from tkinter import *
+win = Tk()
 
-def lotto() :
-    lot = []
+def message(event):
+    lbl['text'] = entry.get()
 
-    for i in range(6) :
-        lot.append(randint(1,45))
-    lot.sort()
-    print(lot)
-
-lotto()
-
-from random import *
-
-def lotto() :
-    lot = set()
-
-    while len(lot) < 6 :
-        lot.add(randint(1,45))#set()에 요소를 추가하는 함수
-        
-    lot = list(lot)
-    lot.sort()
-    print(lot)
-
-lotto()
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-
+entry = Entry(win)
+entry.bind("<Return>",message)
+entry.pack()
+lbl = Label(win,text = " ")
+lbl.pack()
+win.mainloop()
 
 
 
