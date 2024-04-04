cf6764ba272369339b090652256da58300e9dca0
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-04-04 20:59:22 +0900

    2024-04-04

--- abc.py
+++ abc.py
@@ -1,74 +1,41 @@
-from tkinter import *
-from random import *
-
-win =Tk()
-win.title("Rock Scissors Paper game")
-
-def change_img(user) :
-    List = ["scissors.png", "rock.png","paper.png"]
-
-    com = randint(0,2)
-
-    comcom_ing = PhotoImage(file = List[com])
-    useruser_ing = PhotoImage(file = List[user])
-
-    com_img['image'] = comcom_ing
-    com_img.image = comcom_ing
-
-    user_img['image'] = useruser_ing
-    user_img.image = useruser_ing
-    
-    game(com,user)
-
-def game(com,user) :
-     if user == 0:
-         if com == 0:
-             lbl_res['text'] = "Draw"
-         elif com ==1:
-             lbl_res['text'] = "Computer win"
-         else :
-             lbl_res['text'] = "User wins"
-     elif user == 1:
-        if com == 0:
-            lbl_res['text'] = "User wins"
-        elif com == 1:
-            lbl_res['text'] = "Draw"
-        else :
-            lbl_res['text'] = "Computer win"
-     else :
-        if com == 2:
-            lbl_res['text'] = "Draw"
-        elif com == 1:
-            lbl_res['text'] = "User wins"
-        else :
-            lbl_res['text'] = "Computer win"
-        
-        
+####f = open("example.txt","w")
+####f.close()
+####f = open("example.txt","a")
+####alphabet = ['a','b','c','d','e']
+####for c in alphabet :
+####    f.write(c)
+####f.close()
+####f = open("example.txt","r")
+####data = f.read()
+####print(data)
+####f.close()
+####f = open("example.txt","r")
+####while True :
+####    line = f.readline()
+####    if not line : break
+####    print(line,end = '')
+####f.close()
+####f = open("example.txt","r")
+####data = f.readlines()
+####for line in data :
+####    print(line,end = '')
+####f.close()
+##f = open("example.txt","r")
+##while True :
+##    print(f.tell(), end = ' ')
+##    line = f.readline()
+##    print(line.strip())
+##    if not line : break
+##f.seek(26)
+##print("after setting apointer : %d(%s)" % (f.tell(), f.read()[0]))
+##f.close()
+f = open("profile.txt","w")
+name = input("Name : ")
+age = input("Age : ")
+f.write("Name : %s\n" % name)
+f.write("Age : %s\n" % age)       
+f.close()
 
 
-        
 
-    
-    
-basic_img = PhotoImage(file = "ready.png")
 
-com_img = Label(win,image = basic_img,relief = "solid")
-user_img = Label(win,image = basic_img,relief = "solid")
-lbl_com = Label(win, text = "Computer")
-lbl_user = Label(win, text = "User")
-lbl_res = Label(win, text = " ",width = 15,fg = "brown" ,bg = "lightyellow")
-
-btn_scissors = Button(win,text = "scissors",bg = "skyblue",width = 15,command = lambda : change_img(0))
-btn_rock = Button(win,text = "rock",bg = "pink",width = 15,command = lambda : change_img(1))
-btn_paper = Button(win,text = "paper",bg = "light green",width = 15,command = lambda : change_img(2))
-
-com_img.grid(row = 0,column = 0)
-lbl_res.grid(row = 0,column = 1)
-user_img.grid(row = 0,column = 2)
-lbl_com.grid(row = 1,column = 0)
-lbl_user.grid(row = 1,column = 2)
-
-btn_scissors.grid(row = 2, column = 0)
-btn_rock.grid(row = 2,column = 1)
-btn_paper.grid(row =2,column = 2)
-win.mainloop()
