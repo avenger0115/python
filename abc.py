fed87a32f06cfccc372f0637c57d0b0275f36c42
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-04-04 19:57:02 +0900

    20224-04-04

--- abc.py
+++ abc.py
@@ -1,86 +1,55 @@
-##from tkinter import *
-##
-##win = Tk()
-##
-##def click() :
-##
-##     if btn['text'] == "hello" :
-##         btn['text'] = "python"
-##         btn['bg'] = "green"
-##     else :
-##         btn['text'] = "hello"
-##         btn['bg'] = "orange"
-##         
-##         
-##btn = Label(win,text = "hello", fg = "white", bg = "orange")
-##btn2 = Button(win,text = "Button",command = click)
-##btn.pack()
-##btn2.pack()
-##win.mainloop()
-##
-##from tkinter import *
-##win = Tk()
-##win.title("red")
-##abc1 = Label(win, width = 10,height = 5,bg = "red")
-##abc2 = Label(win, width = 10,height = 5,bg = "orange")
-##abc3 = Label(win, width = 10,height = 5,bg = "yellow")
-##abc4 = Label(win, width = 10,height = 5,bg = "green")
-##abc5 = Label(win, width = 10,height = 5,bg = "blue")
-##abc6 = Label(win, width = 10,height = 5,bg = "purple")
-##
-##abc1.grid(row = 0,column = 0)
-##abc2.grid(row = 0,column = 1)
-##abc3.grid(row = 0,column = 2)
-##abc4.grid(row = 1,column = 0)
-##abc5.grid(row = 1,column = 1)
-##abc6.grid(row = 1,column = 2)
-##
-##win.mainloop()
-
-##add = lambda x,y : x+y
-##print(add(10,100))
-##
-##def  remains(a,b) :
-##     return a%b
-##remains = lambda a,b : a%b
-##print(remains(10,3))
-##
-##say = lambda name :print("hello "+name)
-##say("noonoo")
-##
-##from tkinter import *
-##
-##def Click(shape) :
-##    if shape == "circle" :
-##        img = pPhotoLmage(file = "circle.png")
-##    elif shape == "triangle" :
-##        img = PhotoLmage(file = "triangle.png")
-##    else :
-##        img = Photolmage(file = "star.png")
-##
-##    lbl['image'] =img
-##    lbl.image = img
-##
-##win = Tk()
-##
-##img = PhotoImage(file = "circle.png")
-##lbl = Label(win,image = img)
-##
-##btn1 = Button(win,text = "circle",command = lambda : Click("circle"))
-##btn2 = Button(win,text = "triangle",command = lambda : Click("triangle"))
-##btn3 = Button(win,text = "star",command = lambda : Click("star"))
-##
-##lbl.grid(row = 0,column = 0, columnspan = 3)
-##btn1.grid(row = 1,column = 0)
-##btn2.grid(row = 1,column = 1)
-##btn3.grid(row = 1,column = 2)
-##
-##win.mainloop()
-
 from tkinter import *
+from random import *
+
 win =Tk()
 win.title("Rock Scissors Paper game")
 
+def change_img(user) :
+    List = ["scissors.png", "rock.png","paper.png"]
+
+    com = randint(0,2)
+
+    comcom_ing = PhotoImage(file = List[com])
+    useruser_ing = PhotoImage(file = List[user])
+
+    com_img['image'] = comcom_ing
+    com_img.image = comcom_ing
+
+    user_img['image'] = useruser_ing
+    user_img.image = useruser_ing
+    
+    game(com,user)
+
+def game(com,user) :
+     if user == 0:
+         if com == 0:
+             lbl_res['text'] = "Draw"
+         elif com ==1:
+             lbl_res['text'] = "Computer win"
+         else :
+             lbl_res['text'] = "User wins"
+     elif user == 1:
+        if com == 0:
+            lbl_res['text'] = "User wins"
+        elif com == 1:
+            lbl_res['text'] = "Draw"
+        else :
+            lbl_res['text'] = "Computer win"
+     else :
+        if com == 2:
+            lbl_res['text'] = "Draw"
+        elif com == 1:
+            lbl_res['text'] = "User wins"
+        else :
+            lbl_res['text'] = "Computer win"
+        
+        
+
+
+        
+
+    
+    
 basic_img = PhotoImage(file = "ready.png")
 
 com_img = Label(win,image = basic_img,relief = "solid")
@@ -90,8 +59,8 @@ lbl_user = Label(win, text = "User")
 lbl_res = Label(win, text = " ",width = 15,fg = "brown" ,bg = "lightyellow")
 
 btn_scissors = Button(win,text = "scissors",bg = "skyblue",width = 15,command = lambda : change_img(0))
-btn_rock = Button(win,text = "scissors",bg = "pink",width = 15,command = lambda : change_img(0))
-btn_paper = Button(win,text = "scissors",bg = "light green",width = 15,command = lambda : change_img(0))
+btn_rock = Button(win,text = "rock",bg = "pink",width = 15,command = lambda : change_img(1))
+btn_paper = Button(win,text = "paper",bg = "light green",width = 15,command = lambda : change_img(2))
 
 com_img.grid(row = 0,column = 0)
 lbl_res.grid(row = 0,column = 1)
@@ -103,24 +72,3 @@ btn_scissors.grid(row = 2, column = 0)
 btn_rock.grid(row = 2,column = 1)
 btn_paper.grid(row =2,column = 2)
 win.mainloop()
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
