2555b46776c069cffa14631dc4f63d738b8bcccc
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-04-11 20:51:50 +0900

    2024-04-11

--- abc.py
+++ abc.py
@@ -1,40 +1,73 @@
-####f = open("example.txt","w")
-####f.close()
-####f = open("example.txt","a")
-####alphabet = ['a','b','c','d','e']
-####for c in alphabet :
-####    f.write(c)
-####f.close()
-####f = open("example.txt","r")
-####data = f.read()
-####print(data)
-####f.close()
-####f = open("example.txt","r")
-####while True :
-####    line = f.readline()
-####    if not line : break
-####    print(line,end = '')
-####f.close()
-####f = open("example.txt","r")
-####data = f.readlines()
-####for line in data :
-####    print(line,end = '')
-####f.close()
-##f = open("example.txt","r")
-##while True :
-##    print(f.tell(), end = ' ')
-##    line = f.readline()
-##    print(line.strip())
-##    if not line : break
-##f.seek(26)
-##print("after setting apointer : %d(%s)" % (f.tell(), f.read()[0]))
+##f = open("alphabet.txt", "w")
+##f.write("abcdefghijklmnopqrstuvwxyz")
 ##f.close()
-f = open("profile.txt","w")
-name = input("Name : ")
-age = input("Age : ")
-f.write("Name : %s\n" % name)
-f.write("Age : %s\n" % age)       
-f.close()
+##index = int(input("바꿀 포인터의 위치를 입력하세요.:"))
+##f = open("alphabet.txt","r")
+##f.seek(index)
+##date = f.read()
+##print(date)
+##f.close()
+##f = open("fruit.txt","r")
+##word = f.readlines()
+##for i in word:
+##    i = i.strip()
+##    if len(i)>=10:
+##        print(i)
+##f.close()
+##f = open("anna.txt","r")
+##data = f.readline()
+##data = data.split()
+##for i in data:
+##    if 'b' in i:
+##        print(i)
+##f.close()
+def read_file(file_name) :
+    f = open(file_name,"r")
+    lines = f.readlines()
+    for line in lines :
+        print(line.strip())
+    f.close()    
+
+def write_file(file_name, mode) :
+    f = open(file_name,mode)
+    while True:
+        data = input()
+        if data =="q":
+            break
+        f.write(data + "\n")
+    f.close()
+        
+
+file_name = input("File name : ")
+mode = input("File mode(r/w/a) : ")
+
+if mode == "r" :
+    read_file(file_name)
+else :
+    write_file(file_name,mode)
+
+h = int(input("시간 입력 :"))
+m = int(input("분 입력 :"))
+m = 45
+
+if m < 0:
+    h = 1
+    m += 60
+if h < 0 :
+    h+=24
+print("%d %d" %(h,m))
+
+        
+
+
+
+
+
+
+
+
+
+
 
 
 
