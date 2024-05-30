47719f108bfe7d4b8e5d07da583307ac9c125a3a
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-05-30 20:51:21 +0900

    2024-05-30

---
+++ main.py
@@ -0,0 +1,64 @@
+##from calculator import *
+##add(15,5)
+##
+##from calculator import *
+##
+##x=int(input("x : "))
+##y=int(input("y : "))
+##
+##add(x,y)
+##sub(x,y)
+##mul(x,y)
+##temp = input().split()
+##print(int(temp[0]) * int(temp[1]))
+
+##x = int(input())
+##n = int(input())
+##sum = 0
+##
+##for i in range(n):
+##    temp = input().split()
+##    sum +=intint(temp[0]) * int(temp[1])
+##
+##if(sum==x) :
+##    print("yes")
+##else:
+##    print("no")
+
+##n = input().split()
+##a = n.count('1')
+##
+##if a==4:
+##    print("e 모") 
+##elif a==3:
+##    print("a 도")
+##elif a==2:
+##    print("b 개")
+##elif  a==1:
+##    print("c 걸")
+##elif a==0:
+##    print("d 윶")
+##else:
+##    print("입력 오류")
+
+n = int(input())
+m = int(input())
+flag = 0
+for i in range(n,m+1):
+    flag = 0
+    for i in range(2,i) :
+        if i % j == 0:
+            check = 1
+            break
+    if check == 0:
+        print(i,end = ' ')
+    
+
+
+
+    
+    
+    
+
+
+
